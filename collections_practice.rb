915854d77f37916277acb87263ff9414d8c62a7e
Brittany Hartmire <bkhartmire@gmail.com>
2017-11-18 23:22:55 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -37,6 +37,10 @@ end
 
 def add_s(array)
   array.each_with_index.collect do |element, index|
-    element << "s" if index != 1
+    if index != 1
+      element << "s" 
+    else
+      element
+    end
   end
 end
