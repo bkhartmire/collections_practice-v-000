eed73a265af8a998a9689a79e549e83846961728
Brittany Hartmire <bkhartmire@gmail.com>
2017-11-18 23:12:31 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -17,3 +17,15 @@ end
 def reverse_array(array)
   array.reverse
 end
+
+def kesha_maker(array)
+  new_array = []
+  array.each do |string|
+    string[2] = "$"
+    new_array << string
+  end
+end
+
+def find_a(array)
+  array.select {|string| string.start_with?("a")}
+end
